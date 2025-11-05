150c32d8fff81024dc6a013f3783036661e88afb
Mason Daugherty <github@mdrxy.com>
2025-11-05 11:55:05 -0500

    .

--- libs/community/langchain_community/document_loaders/parsers/language/csharp.py
+++ libs/community/langchain_community/document_loaders/parsers/language/csharp.py
@@ -27,7 +27,7 @@ class CSharpSegmenter(TreeSitterSegmenter):
     def get_language(self) -> "Language":
         from tree_sitter_language_pack import get_language
 
-        return get_language("c_sharp")
+        return get_language("csharp")
 
     def get_chunk_query(self) -> str:
         return CHUNK_QUERY
